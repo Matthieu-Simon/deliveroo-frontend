2dff0dd1d199638b712ea91abadefdedffc015af
Matthieu-Simon <simon.matthieu89@gmail.com>
2024-11-06 11:46:42 +0100

    Ajout du Header & Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
 import './App.css';
+import Header from "./components/Header";
 
 function App() {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-  return (
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://site--deliveroo-backend--wmhs7j45p7n2.code.run/");
+
+        setData(response.data);
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error.response);
+      }
+    }
+    fetchData();
+  }, []);
+
+  return isLoading ? <span>En cours de chargement...</span> : (
     <>
-      <h1>Hello, World</h1>
+      <Header />
+      <h2>{data.restaurant.name}</h2>
     </>
   )
 }
