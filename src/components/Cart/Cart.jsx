c6b5d050200e834ac80aa4428e8a44671a771be2
Matthieu-Simon <simon.matthieu89@gmail.com>
2024-11-07 13:44:06 +0100

    Style composant Cart done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,45 @@
-import "./Cart.css"
+import "./Cart.css";
+import { CiCircleMinus, CiCirclePlus } from "react-icons/ci";
 
-function Cart() {
+function Cart({ quantity, meal, price, totalHt, frais, total }) {
     return (
         <div className="cart">
-            <span>Ajouter au panier</span>
+
+            <button className="btn--cart">Valider mon panier</button>
+
+            <div className="detail--cart">
+                <div className="quantity">
+                    <span className="modifyQuantity">
+                        <CiCircleMinus />
+                    </span>
+                    <span>{quantity}</span>
+                    <span className="modifyQuantity">
+                        <CiCirclePlus />
+                    </span>
+                </div>
+                <div>
+                    <span>{meal}</span>
+                </div>
+                <div>
+                    <span>{price} €</span>
+                </div>
+            </div>
+
+            <div className="resumePrice--cart">
+                <div className="detailResumePrice top">
+                    <span>Sous-total</span>
+                    <span>{totalHt} €</span>
+                </div>
+                <div className="detailResumePrice bottom">
+                    <span>Frais de livraison</span>
+                    <span>{frais} €</span>
+                </div>
+            </div>
+
+            <div className="totalPrice--cart">
+                <span>Total</span>
+                <span>{total} €</span>
+            </div>
         </div>
     )
 }
