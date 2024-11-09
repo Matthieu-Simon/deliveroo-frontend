b3f4e8410b4dc3ee4dac1872e1266c257f2580cf
Matthieu-Simon <simon.matthieu89@gmail.com>
2024-11-09 09:30:14 +0100

    Gestion panier done

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -4,32 +4,55 @@ import { CiCircleMinus, CiCirclePlus } from "react-icons/ci";
 
 function Cart({ cartItems }) {
     const [cart, setCart] = useState([]);
+    const [isEmpty, setIsEmpty] = useState(true);
     const [subTotal, setSubtotal] = useState(0);
     const [deliveryFee, setDeliveryFee] = useState(2.80);
     const [total, setTotal] = useState(0);
 
+
     useEffect(() => {
-        setCart(cartItems.map(item => ({...item, quantity: 1 })));
-    
+        const groupedCart = cartItems.reduce((acc, item) => {
+            const existingItem = acc.find(cartItem => cartItem.id === item.id);
+            if (existingItem) {
+                existingItem.quantity += 1;
+                return acc;
+            }
+            return [...acc, { ...item, quantity: 1 }];
+        }, []);
+
+        setCart(groupedCart);
+        setIsEmpty(groupedCart.length === 0);
     }, [cartItems]);
 
     useEffect(() => {
         const total = cart.reduce((acc, item) => acc + item.price * item.quantity, 0);
         setSubtotal(total);
-        
         setTotal(total + deliveryFee);
-
     }, [cart, deliveryFee]);
 
     const handleQuantityChange = (index, operation) => {
         const updatedCart = [...cart];
         if (operation === "increment") {
             updatedCart[index].quantity += 1;
-        } else if (operation === "decrement" && updatedCart[index].quantity > 1) {
-            updatedCart[index].quantity -= 1
+        } else if (operation === "decrement") {
+            if (updatedCart[index].quantity > 1) {
+                updatedCart[index].quantity -= 1
+            } else {
+                updatedCart.splice(index, 1);
+            }
         }
-
         setCart(updatedCart);
+        setIsEmpty(updatedCart.length === 0);
+    };
+
+    if (isEmpty) {
+        return (
+            <div className="cart">
+                <div className="empty-cart">
+                    <p>Votre panier est vide</p>
+                </div>
+            </div>
+        )
     };
 
     return (
@@ -37,25 +60,28 @@ function Cart({ cartItems }) {
 
             <button className="btn--cart">Valider mon panier</button>
 
-            {cart.map((item, index) => (
-                <div key={index} className="detail--cart">
-                    <div className="quantity">
-                        <span className="modifyQuantity" onClick={() => handleQuantityChange(index, "decrement")}>
-                            <CiCircleMinus />
-                        </span>
-                        <span>{item.quantity}</span>
-                        <span className="modifyQuantity" onClick={() => handleQuantityChange(index, "increment")}>
-                            <CiCirclePlus />
-                        </span>
+            <div className="detail--container">
+                {cart.map((item, index) => (
+                    <div key={index} className="detail--cart">
+                        <div className="quantity">
+                            <span className="modifyQuantity" onClick={() => handleQuantityChange(index, "decrement")}>
+                                <CiCircleMinus />
+                            </span>
+                            <span>{item.quantity}</span>
+                            <span className="modifyQuantity" onClick={() => handleQuantityChange(index, "increment")}>
+                                <CiCirclePlus />
+                            </span>
+                        </div>
+                        <div>
+                            <span>{item.title}</span>
+                        </div>
+                        <div>
+                            <span>{item.price} €</span>
+                        </div>
                     </div>
-                    <div>
-                        <span>{item.title}</span>
-                    </div>
-                    <div>
-                        <span>{item.price} €</span>
-                    </div>
-                </div>
-            ))}
+                ))}
+            </div>
+            
             <div>
                 <div className="resumePrice--cart">
                     <div className="detailResumePrice top">
